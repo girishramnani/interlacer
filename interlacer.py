6a6b348aa3da3a68a325ad6cfd00dbb343338469
girishramnani <girishramnani95@gmail.com>
2017-04-04 18:10:03 +0530

    managing logging

--- interlacer.py
+++ interlacer.py
@@ -4,7 +4,7 @@ import subprocess
 import itertools
 import os
 
-lg.basicConfig(format='%(levelname)s:%(message)s',level=lg.DEBUG)
+lg.basicConfig(format='%(levelname)s:%(message)s ',level=lg.DEBUG)
 
 def test_ffmpeg():
     exit_code,output = subprocess.getstatusoutput("ffmpeg")
@@ -22,9 +22,9 @@ def parse_args(args):
     arg_parser = argparse.ArgumentParser(prog="interlacer",\
     description="""Interlace two videos into one cronologically
     and alternating for use with active 3d movies""")
-    
+
     parser.add_argument("-in",type=str,help='Input folders (should be two)', nargs="+")
-    parser.add_argument("-out",type=str,required=True)   
+    parser.add_argument("-out",type=str,required=True)
     return parser.parser_args(args)
 
 def rename_folders(folder,name_gen,pressesion=6):
@@ -34,7 +34,7 @@ def rename_folders(folder,name_gen,pressesion=6):
         lg.info("renaming {} to {}".format(old_name,new_name))
         os.rename("{}/{}".format(folder,old_name),"{}/{:07d}{}".format(folder,new_name,extname))
 
-    
+
 def generate_images(inp,out,frate=30,formt="jpg"):
     args = '-y -r {} -i "{}" -qscale 0 "{}/image.%06d.{}"'.format(frate,inp,out,formt)
     lg.debug("Generating the images at {}".format(out))
@@ -49,9 +49,9 @@ even = lambda : itertools.count(0,2)
 
 if __name__ == "__main__":
     test_ffmpeg()
-    generate_images("003 Using the exercises.mp4","output3")
-    generate_images("002 What you should know.mp4","output4")
-    rename_folders("output3",odd())
-    rename_folders("output4",even())
+    # generate_images("003 Using the exercises.mp4","output3")
+    # generate_images("002 What you should know.mp4","output4")
+    # rename_folders("output3",odd())
+    # rename_folders("output4",even())
 
-    
+
